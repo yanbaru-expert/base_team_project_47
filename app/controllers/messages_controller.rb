56aac2f4cc5564eb13eaa5e889b884b68814d81a
yuukinakamoo <72909522+yuukinakamoo@users.noreply.github.com>
2021-01-01 18:09:00 +0900

    新規投稿機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,16 @@
 class MessagesController < ApplicationController
+
+  def new
+    @message = Message.new
+  end
+
+   def create
+    User.create(user_params)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:title, :content)
+  end
+
 end

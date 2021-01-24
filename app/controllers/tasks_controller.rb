1e448129824c758ab2ddf5f270d345b5c25d4dab
hirekakusi6000 <scorpion4773@icloud.com>
2021-01-24 14:57:54 +0900

    編集機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,6 +7,15 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+  end
+
   def new
     @task = Task.new
   end
